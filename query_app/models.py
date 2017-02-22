75035abff8a5cf80cd103a6406b6a8b0f8de1aae
Yang Ruodong <yangruodong@gmail.com>
2017-02-22 10:52:47 +0800

    change to pyCharm Pro

--- query_app/models.py
+++ query_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+    placeid = models.TextField()
+    formatted_address = models.TextField()
+    vicinity_address = models.TextField()
+    location_lat = models.FloatField()
+    location_lng = models.FloatField()
+    icon = models.URLField()
+    name = models.TextField()
+    phone_number = models.TextField()
+    rating = models.FloatField()
+    permanently_closed = models.BooleanField()
+    url = models.URLField()
+    website = models.URLField()
+
+    def __str__(self):
+        return self.name
